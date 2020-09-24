f9c0986301067de95a08b61d6769fb4b9f6c6928
Cristina Nguyen <cristinanguyen@MacBook-Pro.local>
2020-09-24 12:07:44 -0700

    [calc] add simple functions for a calculator

---
+++ calc.py
@@ -0,0 +1,20 @@
+#test driven development - write tests first and then write code to pass those tests
+
+def add(x, y):
+    """add function"""
+
+    return x + y
+def subtract(x, y):
+    """subtract function"""
+
+    return x + y
+
+def multiply(x, y):
+    """multiply function"""
+
+    return x + y
+
+def divide(x, y):
+    """divide function"""
+
+    return x + y
