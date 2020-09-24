cd945b6b1889fe2bb5035eaa3a5139c3e7219987
Cristina Nguyen <cristinanguyen@MacBook-Pro.local>
2020-09-24 15:17:21 -0700

    add multiple tests

--- test_employee.py
+++ test_employee.py
@@ -4,13 +4,33 @@ from employee import Employee, cristina
 class TestEmployee(unittest.TestCase):
     """Test for Employee class"""
 
-    def test_email(self):
-        levin = Employee('levin', 'batallones', 2000000)
-        martin = Employee('martin', 'briceno', 1999999)
+    @classmethod
+    def setUpClass(cls):
+        print('setupClass')
 
-        self.assertEqual(levin.email, 'levin.batallones@sei713.com')
-        self.assertEqual(martin.email, 'martin.briceno@sei713.com')
-        self.assertEqual(cristina.email, 'cristina.nguyen@sei713.com')
+    def setUp(self):
+        print('setup')
+        self.levin = Employee('levin', 'batallones', 2000000)
+        self.martin = Employee('martin', 'briceno', 1999999)
+        self.cristina = Employee('cristina', 'nguyen', 100000)
+
+    def tearDown(self):
+        print('tearDown\n')
+
+
+    def test_email(self):
+        self.assertEqual(self.levin.email, 'levin.batallones@sei713.com')
+        self.assertEqual(self.martin.email, 'martin.briceno@sei713.com')
+        self.assertEqual(self.cristina.email, 'cristina.nguyen@sei713.com')
+
+    def test_fullname(self):
+        self.assertEqual(self.cristina.fullname, 'cristina nguyen')
+    
+    def test_raise_amount(self):
+        
+        self.cristina.apply_raise()
+
+        self.assertEqual(self.cristina.pay, 115000)
 
 if __name__ == '__main__':
     unittest.main()
